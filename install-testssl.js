333b87a946eab2cc406f2002b022f1d2835ebdc0
vitaliytv <vitaliytv@nitralabs.com>
2020-02-06 06:32:30 +0200

    make dir

--- install-testssl.js
+++ install-testssl.js
@@ -1,5 +1,11 @@
 const https = require("https");
 const tar = require("tar");
+const fs = require("fs");
+const dir = "./testssl";
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+}
 
 https.get("https://codeload.github.com/drwetter/testssl.sh/tar.gz/3.0", function(response) {
   response.pipe(
